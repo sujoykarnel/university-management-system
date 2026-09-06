17913f69a3f6dbac63e1ed1f34a6e7c48bc2b4b9
Sujoy Das <sujoykarnel@gmail.com>
2026-09-06 12:44:24 +0600

    feat: utils added

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,34 @@
-import express, { Application, Request, Response } from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, {
+	type Application,
+	type Request,
+	type Response,
+} from "express";
+import httpStatus from "http-status";
+import config from "./app/config";
 
+const app: Application = express();
 
-const app: Application = express()
+app.use(
+	cors({
+		origin: config.frontend_url,
+		credentials: true,
+	}),
+);
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World")
-})
+// enable url-enccode
+app.use(express.urlencoded({ extended: true }));
 
-export default app
+// middleware
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", async (req: Request, res: Response) => {
+	res.send(httpStatus.OK).json({
+		success: true,
+		message: "Welcome University Management System Backend",
+	});
+});
+
+export default app;
