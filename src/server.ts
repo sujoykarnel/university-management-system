fb4c330374ce446a84fb3f4d37e6cea99f38b988
Sujoy Das <sujoykarnel@gmail.com>
2026-09-18 22:54:39 +0600

    Feat: depandancy added

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,33 @@
-import app from "./app"
+import app from "./app";
+import config from "./app/config";
+import { prisma } from "./app/lib/prisma";
 
-const PORT = process.env.PORT || 5000
+const PORT = config.port;
 
 async function main() {
-  try {
-    app.listen(PORT, () => {
-      console.log(`Server is Running on Port ${PORT}`);
-    })
+	try {
+    // database
+		await prisma.$connect();
+		console.log("Database Connected Successfully");
 
-  } catch (error) {
-    console.log(`Error Starting the Server:`, error);
-    process.exit(1)
-  }
+    // redis
 
+
+    // mail transporter
+
+    // init seeding
+
+
+    // delete unverified
+
+		app.listen(PORT, () => {
+			console.log(`Server is Running on Port ${PORT}`);
+		});
+	} catch (error) {
+		console.log(`Error Starting the Server:`, error);
+		await prisma.$disconnect();
+		process.exit(1);
+	}
 }
 
-main()
+main();
