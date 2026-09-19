d134df02fa9f74078645ee708b807b6e6a06ecdf
Sujoy Das <sujoykarnel@gmail.com>
2026-09-19 09:36:15 +0600

    feat: auth check & global error handler done

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,8 @@ import express, {
 } from "express";
 import httpStatus from "http-status";
 import config from "./app/config";
+import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
+import { AuthRouters } from "./app/module/auth/auth.route";
 
 const app: Application = express();
 
@@ -24,11 +26,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
+// routes
+app.use("/api/v1/auth", AuthRouters);
+
 app.get("/", async (req: Request, res: Response) => {
 	res.send(httpStatus.OK).json({
 		success: true,
 		message: "Welcome University Management System Backend",
 	});
 });
+
+// global error handler
+app.use(globalErrorHandler);
+
+
+// not found
 
 export default app;
--- src/app/module/auth/auth.route.ts
+++ src/app/module/auth/auth.route.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { AuthController } from "./auth.controller";
 
-const router = Router()
+const router = Router();
 
-router.post('/google', AuthController.googleLogin)
+router.post("/google", AuthController.googleLogin);
+
+export const AuthRouters = router;
